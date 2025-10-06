3c1a6e82af8d82fd6d93c58d89a8290f66abc3c4
RohitAnandNitk <sharmarohit64748@gmail.com>
2025-10-06 23:33:03 +0530

    deployed frontend url added in the cors at backend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,22 +1,19 @@
-import dotenv from 'dotenv' ;
-   dotenv.config()  ; 
+import dotenv from "dotenv";
+dotenv.config();
 
-import express from 'express' ; 
-import cors from 'cors' ;
-import cookieParser from 'cookie-parser' ;
-import { connectDB } from './lib/db.js';
-import authRoutes from "./routes/authRoute.js"
-import blogRoutes from './routes/blogRoute.js';
-import contactRoute from './routes/contactRoute.js';
-import productRoute from "./routes/productRoute.js" ; 
-import cartRoute from "./routes/cartRoute.js" ; 
-import reviewRoute from './routes/reviewRoute.js';
-import userRoute from './routes/userRoute.js';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/authRoute.js";
+import blogRoutes from "./routes/blogRoute.js";
+import contactRoute from "./routes/contactRoute.js";
+import productRoute from "./routes/productRoute.js";
+import cartRoute from "./routes/cartRoute.js";
+import reviewRoute from "./routes/reviewRoute.js";
+import userRoute from "./routes/userRoute.js";
 
-
-
-
-const app  = express () ; 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
@@ -26,20 +23,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // cors origin allow from everywhere
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://www.psrenterprises.store/"],
+    credentials: true,
+  })
+);
 
-
-app.use("/api/auth" , authRoutes)
-app.use("/api/blog" , blogRoutes)
-app.use("/api/contact" , contactRoute)
-app.use("/api/products" , productRoute) ; 
-app.use("/api/cart" , cartRoute) ;
-app.use("/api/reviews" , reviewRoute) ;
-app.use("/api/users" , userRoute) ;
-
+app.use("/api/auth", authRoutes);
+app.use("/api/blog", blogRoutes);
+app.use("/api/contact", contactRoute);
+app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/reviews", reviewRoute);
+app.use("/api/users", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
